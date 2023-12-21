fb37e2da6a714d0c79ad599dd260fa9aac08cef9
Daniela <danielapb78@gmail.com>
2023-12-21 17:40:23 -0500

    implementando controlador

---
+++ src/test/java/com/codelabGit/codelabGit/CodelabGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelabGit.codelabGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelabGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
